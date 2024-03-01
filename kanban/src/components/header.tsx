1fccb4765d57a630ec50f13ecff747799e218acf
akin0ri <akinori.u.2003.jp3@gmail.com>
2024-03-01 15:48:40 +0000

    :lipstick: 余白調整

--- kanban/src/components/header.tsx
+++ kanban/src/components/header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
     <div className="p-3 border-bottom border-3">
       <h1>Kanban</h1>
       <div>
-        <button type="button" className="btn btn-primary">
+        <button type="button" className="me-3 btn btn-primary">
           My Page
         </button>
         <button type="button" className="btn btn-primary">

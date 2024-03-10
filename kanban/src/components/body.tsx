2e031b7ea50b2bb1b7638da8081d3f1e26a64f81
akin0ri <akinori.u.2003.jp3@gmail.com>
2024-03-10 13:25:48 +0000

    :lipstick: Update function to handle edge cases

---
+++ kanban/src/components/body.tsx
@@ -0,0 +1,5 @@
+import { DndContext } from '@dnd-kit/core'
+
+export default function Body() {
+  return <DndContext></DndContext>
+}
---
+++ kanban/src/components/itemtable.tsx
@@ -0,0 +1,5 @@
+export default function ItemTable() {
+  return (
+
+  )
+}
